98dd3ec1d43840281bfc32758d41ae7549ff6003
Gaétan Gellens <gagazor3@gmail.com>
2021-05-16 22:46:07 +0200

    first commit

---
+++ train_chatbot.py
@@ -0,0 +1,99 @@
+import nltk
+#pip install tensorflow
+
+#from tensorflow import keras
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']  
+data_file = open('intents.json').read()
+intents = json.loads(data_file)
+
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+
+        # take each word and tokenize it
+        w = nltk.word_tokenize(pattern)   #It tokenizes the sentence
+        words.extend(w)                   #Extend list by appending elements from the iterable
+        # adding documents
+        documents.append((w, intent['tag'])) #there are 14 tags as a whole
+
+        # adding classes to our class list
+        if intent['tag'] not in classes:  #there are 13 tags but less classes because some of them do not have patterns -> they are not taken into account in the loop
+            classes.append(intent['tag'])
+
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words] #cancel plural and so on. Works with words, not list
+words = sorted(list(set(words)))  #alphabet order
+
+classes = sorted(list(set(classes)))
+
+print (len(documents), "documents")
+
+print (len(classes), "classes", classes)
+
+print (len(words), "unique lemmatized words", words)
+
+
+pickle.dump(words,open('words.pkl','wb'))  #That's two other files
+pickle.dump(classes,open('classes.pkl','wb'))
+
+# initializing training data
+training = []
+output_empty = [0] * len(classes) #[0, 0, 0, 0, 0, 0, 0, 0, 0]
+for doc in documents:
+    # initializing bag of words
+    bag = [] #initialized 47 times
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]  #Every sentences
+    # lemmatize each word - create base word, in attempt to represent related words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0) #check for every words of the dictionary. Else 0
+
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])  #the list with 0000010001110 (e.g) and the specified class
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+# create train and test lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
+
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)  #that's the application of the model
+model.save('chatbot_model.h5', hist)
+
+print("model created")
